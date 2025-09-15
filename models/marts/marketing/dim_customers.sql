3a3acc2fc00cb007cf1955464284802e58add555
olusolaolaide.nathan@gmail.com <olusolaolaide.nathan@gmail.com>
2025-09-15 02:21:14 +0000

    developed my fct_orders model and adding customer lifetime value to my dim_customers

--- models/marts/marketing/dim_customers.sql
+++ models/marts/marketing/dim_customers.sql
@@ -6,14 +6,15 @@ with customers as(
 orders as (
     select
         * 
-    from {{ ref('stg_jaffle_shop__orders') }}
+    from {{ ref('fct_orders') }}
 ),
 customer_orders as (
     select
         customer_id,
         min(order_date) as first_order_date,
         max(order_date) as most_recent_order_date,
-        count(order_id) as number_of_orders
+        count(order_id) as number_of_orders,
+        sum(amount) as lifetime_value
     from orders
     group by 1
 ), 
@@ -24,7 +25,8 @@ final as (
         customers.last_name,
         customer_orders.first_order_date,
         customer_orders.most_recent_order_date,
-        coalesce(customer_orders.number_of_orders, 0) as number_of_orders
+        coalesce(customer_orders.number_of_orders, 0) as number_of_orders,
+        customer_orders.lifetime_value
     from customers
     left join customer_orders 
     on customers.customer_id = customer_orders.customer_id
